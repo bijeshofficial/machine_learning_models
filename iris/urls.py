19ed0c65348cf90574acee32ff55b14b98f837cd
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-07-26 22:43:18 +0545

    changed form to get data using post method

--- iris/urls.py
+++ iris/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'iris'
+# app_name = 'iris'
 
 urlpatterns = [
     path('', views.iris,  name = 'iris'),
-    path('iris/', views.predict_changes, name = 'submit_prediction'),
 ]
--- iris/views.py
+++ iris/views.py
@@ -1,28 +1,37 @@
 from django.shortcuts import render
 import pandas as pd
-from django.http import JsonResponse
 
 # Create your views here.
 def iris(request):
-    return render(request, 'iris/iris.html')
-
-def predict_changes(request):
-    if request.method:
+    if request.method == 'POST':
         #Receive data from the client
-        sepal_length = float(request.POST.get('sepal_length'))
-        sepal_width = float(request.POST.get('sepal_width'))
-        petal_length = float(request.POST.get('petal_length'))
-        petal_width = float(request.POST.get('petal_width'))
+        sepal_length = request.POST.get('sepal_length')
+        sepal_width = request.POST.get('sepal_width')
+        petal_length = request.POST.get('petal_length')
+        petal_width = request.POST.get('petal_width')
+
+        # print(type(sepal_length), sepal_width, petal_length, petal_width)
     
         #Unpickle the model
-        model = pd.read_pickle('pickles/iris_scv.pkl')
+        model = pd.read_pickle('./iris/pickles/iris_scv.pkl')
+
+        print(model)
 
         #Make prediction
-        result = model.predict([[2.1,2.2,2.1,2.1]])
+        result = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])
 
         #Saved the prediction in this new variable
         classification = result[0]
 
-        return JsonResponse({
-            'result':classification, 'sepal_length': sepal_length, 'sepal_width': sepal_width,'petal_length': petal_length, 'petal_width': petal_width,
-        },safe = False)
+        print(classification)
+
+        # context = {
+        #     'classification': classification,
+        # }
+
+        # return render(request, 'iris/iris.html', context)
+
+    else:
+        return render(request, 'iris/iris.html')
+
+    
