d72a9a9b160819b7fb4522b9967eaeb27a25d95c
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-08-12 19:13:58 +0545

    added bank_approval function

--- bank_loan/views.py
+++ bank_loan/views.py
@@ -9,14 +9,32 @@ from .forms import ApprovalForm
 class ApprovalsView(viewsets.ModelViewSet):
     queryset = Approval.objects.all()
     serializer_class = approvalsSerializers
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
 
 
 def bank_approval(request):
-    form  = ApprovalForm()
+    form  = ApprovalForm(None or request.POST)
+    if form.is_valid():
+        first_name = form.cleaned_data['first_name']
+        last_name = form.cleaned_data['last_name']
+        gender = form.cleaned_data['gender']
+        married = form.cleaned_data['married']
+        dependents = form.cleaned_data['dependents']
+        education = form.cleaned_data['education']
+        self_employed = form.cleaned_data['self_employed']
+        applicant_income = form.cleaned_data['application_income']
+        co_applicant_income = form.cleaned_data['co_applicant_income']
+        loan_amount = form.cleaned_data['loan_amount']
+        loan_term = form.cleaned_data['loan_term']
+        credit_history = form.cleaned_data['credit_history']
+        property_area = form.cleaned_data['property_area']
+        
+
+        print(firstname)
 
     context = {
         'form' : form,
     }
 
     return render (request, 'bank_loan/bank_loan.html', context)
+    
