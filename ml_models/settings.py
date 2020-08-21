d1bdd221d88bd22b083dbc324c52528480ca9ab2
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-08-21 16:36:45 +0545

    hid secret key

--- ml_models/settings.py
+++ ml_models/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7@*vkzm72%!5s@51cojj@ls!&1snu-jgxntd2i@gu577gky2r('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
